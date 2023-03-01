ab03673563ed5054c7c4988393fdd15bb0f16d85
JeffersonPulido <jeffersonpulido55@gmail.com>
2023-03-01 08:43:57 -0500

    Migracion react v1.4

--- vcard/assets/js/footer.js
+++
@@ -1,9 +0,0 @@
-const getDateFooter = () => {
-    const year = document.getElementById("year");
-    const yearCopy = year.textContent = new Date().getFullYear();
-    year.innerHTML = yearCopy
-}
-
-window.addEventListener("load", (event) => {
-    getDateFooter();
-});
